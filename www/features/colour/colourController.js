5680a245660a701eeec1e04a42711c46a1f48754
unknown <Graham.Simmons@OV00027.oval.local>
2016-07-11 11:52:13 +0100

    Insomnia fix! Simple alarm time with annoying beep!

--- www/features/colour/colourController.js
+++ www/features/colour/colourController.js
@@ -4,6 +4,7 @@ angular.module('kissClock')
 
     $scope.myDate = new Date();
     $scope.myColor = ColourFactory.getColor();
+    $scope.myDarkestColor = ColourFactory.getDarkestColor();
 
     $scope.colorLighter = function() {$scope.myColor = ColourFactory.colorLighter();}
     $scope.colorDarker = function() {$scope.myColor = ColourFactory.colorDarker();}
