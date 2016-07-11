0ce97694336dd7886ec81230e9563ce5f8a25e22
unknown <Graham.Simmons@OV00027.oval.local>
2016-07-11 19:09:38 +0100

    Removed alerts.

--- platforms/browser/www/features/time/timeController.js
+++ platforms/browser/www/features/time/timeController.js
@@ -3,10 +3,8 @@ angular.module('kissClock')
 .controller('TimeCtrl', function($ionicPlatform, $scope, $state, $interval, TimeFactory) {
 
     // $ionicPlatform.ready(function() {
-        alert('TimeCtrl called.');
         var tick = function() {
             $scope.clock = Date.now();
-            alert($scope.clock);
         }
         tick();
         $interval(tick, 1000);
