c1fd2ea28f25684f60a1318f1ddf12a8eff9d2b0
unknown <Graham.Simmons@OV00027.oval.local>
2016-07-11 13:39:42 +0100

    Basic working example.

--- www/features/time/timeController.js
+++ www/features/time/timeController.js
@@ -1,35 +1,11 @@
 angular.module('kissClock')
 
-.controller('TimeCtrl', function($ionicPlatform, $scope, $state, $interval, $cordovaToast, TimeFactory, AlarmFactory) {
+.controller('TimeCtrl', function($ionicPlatform, $scope, $state, $interval, $cordovaToast, TimeFactory) {
 
     $ionicPlatform.ready(function() {
 
-        // window.plugins.insomnia.keepAwake(
-        //     function(msg) {console.log(msg)}
-        // );
-
-        // var alarmActive = (alarmActive == undefined ? AlarmFactory.isEnabled() : alarmActive);
-        // var dtAlarm = (dtAlarm == undefined ? null : dtAlarm);
-        //
-        // if(alarmActive && dtAlarm == null) {
-        //     dtAlarm = AlarmFactory.getAlarmTime();
-        // }
-
         var tick = function() {
-            dt = new Date();
-            $scope.clock = dt;
-
-            // dts = dt.toLocaleString();
-            // if(alarmActive && dts == dtAlarm) {
-            //     navigator.notification.beep(1);
-            //     navigator.notification.alert(
-            //                                 'Now what?',        // message
-            //                                 function(){},       // callback
-            //                                 'Alarm Actiated',   // title
-            //                                 'OK'                // buttonName
-            //                                 );
-            // }
-
+            $scope.clock = Date.now();
         }
 
         tick();
@@ -38,24 +14,6 @@ angular.module('kissClock')
         $scope.showHours = TimeFactory.showHours();
         $scope.showSeconds = TimeFactory.showSeconds();
         $scope.showConfig = function() {$state.go("config");}
-
-        // $cordovaToast.show('Here is a message', 'long', 'center').then(function(success) {
-        //     // success
-        // }, function (error) {
-        //     // error
-        // });
-        //
-        // $cordovaToast.showShortTop('Here is a message').then(function(success) {
-        //     // success
-        // }, function (error) {
-        //     // error
-        // });
-        //
-        // $cordovaToast.showLongBottom('Here is a message').then(function(success) {
-        //     // success
-        // }, function (error) {
-        //     // error
-        // });
 
     });
 
--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('kissClock', ['ionic', 'ngCordova', 'ionic-color-picker', 'ngFitText', 'ionic-modal-select'])
+angular.module('kissClock', ['ionic', 'ngCordova', 'ionic-color-picker', 'ngFitText'])
 
 .run(function($ionicPlatform) {
 
