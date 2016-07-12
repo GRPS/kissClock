2ecf70f3e6dc8e23523aef7cfe1e5e71863ba040
unknown <Graham.Simmons@OV00027.oval.local>
2016-07-12 12:36:11 +0100

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -21,29 +21,6 @@ angular.module('kissClock', ['ionic', 'ngCordova', 'ionic-color-picker', 'ngFitT
             ionic.Platform.isFullScreen = true;
         }
 
-        alert('app.js start');
-        DBA.init()
-            .then(function() {
-                alert('start prepare');
-                alert(db);
-                alert('prepare go now');
-                return DBA.prepareTables();
-            })
-            .then(function(cnt) {
-                alert('cnt = ' + cnt);
-                if(cnt == 0) {
-                    DBA.query("INSERT INTO Config (key, obj) values (?, ?)", ["config", angular.toJson(sharedData)]);
-                } else {
-                    DBA.query("SELECT c.rowid AS id, c.key AS key, c.obj AS obj FROM Config c WHERE c.rowid = 1")
-                        .then(function(result){
-                            res = DBA.getFirst(result);
-                            alert(res.obj);
-                            sharedData = JSON.parse(res.obj);
-                            alert('app.js done');
-                        });
-                }
-            })
-
     });
 
 })
