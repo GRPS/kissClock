c9fa7431aa23f2139d5d93f59962d22d4561dc72
unknown <Graham.Simmons@OV00027.oval.local>
2016-07-13 11:45:19 +0100

    no message

--- platforms/android/assets/www/features/config/configService.js
+++
@@ -1,7 +0,0 @@
-angular.module('kissClock')
-
-.service('config', function() {
-
-    return {}
-
-})
--- www/features/config/configController.js
+++ www/features/config/configController.js
@@ -1,6 +1,6 @@
 angular.module('kissClock')
 
-.controller('ConfigCtrl', function($ionicPlatform, $scope, $state, config) {
+.controller('ConfigCtrl', function($ionicPlatform, $scope, $state, sharedData, DBA, ColourFactory) {
 
     //Do something before the view is rendered. Must be kept to bare minimum, so user is not held waiting necessarily.
     $scope.$on("$ionicView.beforeEnter", function(event, data) {
@@ -10,18 +10,16 @@ angular.module('kissClock')
 
             window.plugins.insomnia.allowSleepAgain();
 
-            $scope.config = config;
+            $scope.config = sharedData;
+            $scope.myColor = sharedData.colour.user;
 
-            // $scope.config = sharedData;
-            // $scope.myColor = sharedData.colour.user;
-            //
-            // $scope.brightnessMin = 0;
-            // $scope.brightnessMax = (sharedData.colour.list).length-1;
-            // $scope.brightnessIndex = sharedData.colour.index;
-            //
-            // $scope.weightnessMin = 0;
-            // $scope.weightnessMax = (sharedData.font.weights).length-1;
-            // $scope.weightnessIndex = sharedData.font.weight;
+            $scope.brightnessMin = 0;
+            $scope.brightnessMax = (sharedData.colour.list).length-1;
+            $scope.brightnessIndex = sharedData.colour.index;
+
+            $scope.weightnessMin = 0;
+            $scope.weightnessMax = (sharedData.font.weights).length-1;
+            $scope.weightnessIndex = sharedData.font.weight;
 
         });
 
@@ -33,19 +31,19 @@ angular.module('kissClock')
         $ionicPlatform.ready(function() {
 
             $scope.back = function() {
-            //
-            //     sharedData.colour.list = [];
-            //     $scope.myColor = ColourFactory.getColor();
-            //     j = angular.toJson(sharedData);
-            //     // alert('saving ... ' + j);
-            //
-            //     DBA.query("DELETE FROM Config;")
-            //         .then(function() {
-            //             DBA.query("INSERT INTO Config (key, obj) values (?, ?)", ["config", j]);
-            //         })
-            //         .then(function() {
+
+                sharedData.colour.list = [];
+                $scope.myColor = ColourFactory.getColor();
+                j = angular.toJson(sharedData);
+                // alert('saving ... ' + j);
+
+                DBA.query("DELETE FROM Config;")
+                    .then(function() {
+                        DBA.query("INSERT INTO Config (key, obj) values (?, ?)", ["config", j]);
+                    })
+                    .then(function() {
                         $state.go("time");
-            //         })
+                    })
             }
 
         });
